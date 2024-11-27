20be14018d8580984410236568888a3dc16810b9
deeb1743 <ahmeddeebtitoz@gmail.com>
2024-11-27 16:05:59 +0300

    init

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+void main(){
+  runApp(const Dalel());
+}
+
+class Dalel extends StatelessWidget {
+  const Dalel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
